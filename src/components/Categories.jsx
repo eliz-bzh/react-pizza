b87107a927e32ce34fafeb19272610e37f4b1622
Your Name <you@example.com>
2021-03-30 02:15:24 +0300

    Components

---
+++ src/components/Categories.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+const Categories = ({ items, onClickItem }) => {
+
+    const [activeItem, setActiveItem] = useState(null);
+
+    return (
+        <div className="categories">
+            <ul>
+                <li className={activeItem === null ? 'active' : ''} onClick={() => setActiveItem(null)}>Все</li>
+                {items && items.map((item, index) =>
+                    <li className={activeItem === index ? 'active' : ''} onClick={() => setActiveItem(index)} key={index}>{item}</li>
+                )}
+            </ul>
+        </div>
+    )
+}
+export default Categories;
