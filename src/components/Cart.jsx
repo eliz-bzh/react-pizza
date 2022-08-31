54a758d63d54dabdfb11b7bf20b36a0d0b1ab073
Liz <bozhkova.elizaveta34@gmail.com>
2022-08-31 22:37:22 +0300

    empty cart and render added pizzas

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,24 +1,26 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import Button from './Button';
+import cartEmptyImage from '../assets/img/empty-cart.png';
+import CartItem from './CartItem';
 
 const CartComponent = () => {
 
     const dispatch = useDispatch();
-    const { items, totalPrice, totalCount } = useSelector(({ cartReducer }) => {
-        return {
-            items: cartReducer.items,
-            totalPrice: cartReducer.totalPrice,
-            totalCount: cartReducer.totalCount
-        }
-    });
-    const [pizzaCart, setPizzaCart] = useState([]);
+    const { items, totalPrice, totalCount } = useSelector(({ cartReducer }) => cartReducer);
 
+    const pizzasCart = Object.keys(items).map((key) => {
+        return items[key];
+      });
+    
+    console.log(pizzasCart)
+    
     return (
         <div className="container">
             <div className="content">
                 <div className="container container--cart">
-                    {(pizzaCart && pizzaCart.length === 0) ? (
+                    { totalCount ? (
                         <div className="cart">
                             <div className="cart__top">
                                 <h2 className="content__title">{/*<% include ../../public/img/cart.svg %>*/} Корзина</h2>
@@ -30,6 +32,12 @@ const CartComponent = () => {
                             <div className="content__items">
                                 { /* <% include components/cart-item.ejs %> <% include components/cart-item.ejs %> <%
               include components/cart-item.ejs %> <% include components/cart-item.ejs %>*/}
+                                {pizzasCart.map((obj)=>
+                                    <CartItem {...obj} totalCount totalPrice/>
+                                )}
+                                
+
+
                             </div>
                             <div className="cart__bottom">
                                 <div className="cart__bottom-details">
@@ -47,7 +55,22 @@ const CartComponent = () => {
                                 </div>
                             </div>
                         </div>
-                    ) : ('Clear')}
+                    ) : (
+                        <div className="cart cart--empty">
+                        <h2>
+                        Корзина пустая <i>😕</i>
+                        </h2>
+                        <p>
+                        Вероятней всего, вы не заказывали ещё пиццу.
+                        <br />
+                        Для того, чтобы заказать пиццу, перейди на главную страницу.
+                        </p>
+                        <img src={cartEmptyImage} alt="Empty cart" />
+                        <Link to="/" className="button button--black">
+                        <span>Вернуться назад</span>
+                        </Link>
+                    </div>
+                  )}
                 </div>
             </div>
         </div>
