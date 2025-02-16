54481cef836e67d4a8ca47a682e5b3a2ba851fc8
Liz <bozhkova.elizaveta34@gmail.com>
2025-02-16 17:40:36 +0300

    add details and refactoring code

--- src/components/PizzaBlock/index.jsx
+++ src/components/PizzaBlock/index.jsx
@@ -1,29 +1,13 @@
-import React, {useState} from 'react';
-import classNames from 'classnames';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { LoadingBlock } from '..';
 import Button from '../Button';
 
 const PizzaBlock = ({ id, images, name, types, prices, ingredients, isLoading, onClickItem, addedCount, handleClickPizza, handleDialogShow }) => {
 
-    const availableTypes = ['традиционное', 'тонкое'];
-    const availableSizes = [25, 30, 35];
-    //const [activeType, setActiveType] = useState(0);
-    const [activeSize, setActiveSize] = useState(types[0].availableSizes[0].size);
-
     if (isLoading) {
         return <LoadingBlock />;
     }
-
-    /*const onAddPizza = () => {
-        const obj = { id, name, imageUrl: images[activeType][activeSize], price: prices[activeSize], size: activeSize, type: availableTypes[activeType] };
-        onClickItem(obj)
-    };*/
-
-    // Установить активный размер на первый доступный, если текущий недоступен
-    /*if (!types[activeType].availableSizes.some(s=>s.size === activeSize)) {
-        setActiveSize(types[activeType].availableSizes[0].size);
-    }*/
 
     return (
         <div className="pizza-block" onClick={()=> {
@@ -36,7 +20,27 @@ const PizzaBlock = ({ id, images, name, types, prices, ingredients, isLoading, o
                 alt="Pizza"
             />
             <h4 className="pizza-block__title">{name}</h4>
+            <div className="pizza-block__ingredients">{ingredients.map((ing, index)=><span key={index}>{!(index === ingredients.length-1) ? `${ing}, ` : ing}</span>)}</div>
 
+            <div className="pizza-block__bottom">
+                <div className="pizza-block__price">от {prices[25]} ₽</div>
+                <Button className='button--add' outline>
+                    <svg
+                        width="12"
+                        height="12"
+                        viewBox="0 0 12 12"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
+                            fill="white"
+                        />
+                    </svg>
+                    <span>Выбрать</span>
+                    {addedCount && <i>{addedCount}</i>}
+                </Button>
+            </div>
         </div>
     )
 }
